bb4d4c00e345c8483d55939e70beac986824a4f1
01-turgut <gilgilturgut@gmail.com>
2022-02-24 10:42:00 +0000

    Add higscores.html and highscores.js files

--- assets/js/end.js
+++ assets/js/end.js
@@ -7,11 +7,13 @@ const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 console.log(highScores)
 finalScore.innerText = mostRecentScore;
 
+// This event listener is for enable the username input when there is any text
 userName.addEventListener('keyup', () => {
 
     saveScoreBtn.disabled = !userName.value;
 });
 
+// this function is for saving all the scores, and push into an array
 saveHighScore = e => {
     console.log('clicked the save button!');
     e.preventDefault();
@@ -20,6 +22,10 @@ saveHighScore = e => {
         score: mostRecentScore,
         name: userName.value
     };
-    highScores.push(score)
-    console.log(score);
+    highScores.push(score);
+    highScores.sort( (a,b) => b.score- a.score);
+    highScores.splice(5);
+    
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('/')
 }
