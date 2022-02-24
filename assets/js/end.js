ec888ecd0c367fb4dc6194f28ab386c8236c1d8f
01-turgut <gilgilturgut@gmail.com>
2022-02-24 11:58:04 +0000

    Add new icons

--- assets/js/end.js
+++ assets/js/end.js
@@ -16,7 +16,7 @@ userName.addEventListener('keyup', () => {
 saveHighScore = e => {
     
     e.preventDefault();
-    
+
     const score = {
         score: mostRecentScore,
         name: userName.value
@@ -26,5 +26,5 @@ saveHighScore = e => {
     highScores.splice(5);
     
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign()
+    window.location.assign('/')
 }
