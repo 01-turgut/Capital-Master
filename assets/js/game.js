a53fb589539fffaf28b3851867359f40ebb04856
01-turgut <gilgilturgut@gmail.com>
2022-03-05 03:01:22 +0000

    Change from hard coded property to an array of choices

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,6 +2,8 @@ const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-container'));
 const questionCounterText = document.getElementById('questnum')
 const scoreText = document.getElementById('score');
+const imgContainer = document.getElementById('imgContainer');
+const gameContainer = document.getElementById('choices-container');
 
 
 
@@ -14,67 +16,86 @@ let availableQuestions = [];
 // Here I will create my questions array
 let questions = [{
         question: 'What\'s the capital of Italy?',
-        choice1: 'Rome',
-        choice2: 'Paris',
-        choice3: 'Venice',
-        choice4: 'Florence',
+        choices: ['Rome', 'Paris', 'Venice', 'Florence'],
         answer: 1,
+        imageURL: "https://images.pexels.com/photos/2064827/pexels-photo-2064827.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
     },
     {
         question: 'What\'s the capital of Spain?',
-        choice1: 'Barcelona',
-        choice2: 'Seville',
-        choice3: 'Madrid',
-        choice4: 'Valancia',
+        choices: ['Barcelona',
+            'Seville',
+            'Madrid',
+            'Valancia'
+        ],
         answer: 3,
+        imageURL: "https://www.austrianblog.com/media/images/moskau-reisefuehrer_qm5oxfp.width-1600.jpg"
+
     },
     {
         question: 'What\'s the capital of Ireland?',
-        choice1: 'Galway',
-        choice2: 'Cork',
-        choice3: 'Belfast',
-        choice4: 'Dublin',
+        choices: ['Galway',
+            'Cork',
+            'Belfast',
+            'Dublin'
+        ],
         answer: 4,
+        imageURL: "https://www.austrianblog.com/media/images/moskau-reisefuehrer_qm5oxfp.width-1600.jpg"
+
     },
     {
         question: 'What\'s the capital of Greece?',
-        choice1: 'Athens',
-        choice2: 'Patras',
-        choice3: 'Larissa',
-        choice4: 'Chania',
+        choices: ['Athens',
+            'Patras',
+            'Larissa',
+            'Chania'
+        ],
         answer: 1,
+        imageURL: "https://www.austrianblog.com/media/images/moskau-reisefuehrer_qm5oxfp.width-1600.jpg"
+
     },
     {
         question: 'What\'s the capital of Germany?',
-        choice1: 'Hamburg',
-        choice2: 'Frankfurt',
-        choice3: 'Berlin',
-        choice4: 'Munich',
+        choices: ['Hamburg',
+            'Frankfurt',
+            'Berlin',
+            'Munich'
+        ],
         answer: 3,
+        imageURL: "https://www.austrianblog.com/media/images/moskau-reisefuehrer_qm5oxfp.width-1600.jpg"
+
     },
     {
         question: 'What\'s the capital of Russia?',
-        choice1: 'Moscow',
-        choice2: 'Saint Petersburg',
-        choice3: 'Nizhny Novgorod',
-        choice4: 'Kazan',
+        choices: ['Moscow',
+            'Saint Petersburg',
+            'Nizhny Novgorod',
+            'Kazan'
+        ],
         answer: 1,
+        imageURL: "https://www.austrianblog.com/media/images/moskau-reisefuehrer_qm5oxfp.width-1600.jpg"
+
     },
     {
         question: 'What\'s the capital of Portugal?',
-        choice1: 'Porti',
-        choice2: 'Coimbra',
-        choice3: 'Lisbon',
-        choice4: 'Braga',
+        choices: ['Porti',
+            'Coimbra',
+            'Lisbon',
+            'Braga'
+        ],
         answer: 3,
+        imageURL: "https://images.pexels.com/photos/4304222/pexels-photo-4304222.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+
     },
     {
         question: 'What\'s the capital of Sweden?',
-        choice1: 'Stockholm',
-        choice2: 'Malmö',
-        choice3: 'Gothenburg',
-        choice4: 'Kiruna',
+        choices: ['Stockholm',
+            'Malmö',
+            'Gothenburg',
+            'Kiruna'
+        ],
         answer: 1,
+        imageURL: "https://www.austrianblog.com/media/images/moskau-reisefuehrer_qm5oxfp.width-1600.jpg"
+
     }
 ];
 
@@ -91,25 +112,32 @@ startGame = () => {
 //This function is for calling the new question whenever user clicks on any choice 
 
 getNewQuestion = () => {
-    if (availableQuestions.length === 0 || questionCounter >= maxQuestions){
-    localStorage.setItem('mostRecentScore', score);
+    //@CC Hide Image . Show Question
+    gameContainer.style.display = "block"
+    imgContainer.style.display = "none"
+    if (availableQuestions.length === 0 || questionCounter >= maxQuestions) {
+        localStorage.setItem('mostRecentScore', score);
         // Go to end page
         return window.location.assign("/end.html");
     }
-// This will count and show the user,  the number of the question they are on
+    // This will count and show the user,  the number of the question they are on
     questionCounter++;
     questionCounterText.innerText = questionCounter + "/" + maxQuestions;
 
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-     currentQuestion = availableQuestions[questionIndex];
+    currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
 
-
+    //@CC added index variable for the answers
+    var idx = 0;
     choices.forEach(choice => {
+        console.log(choice)
         const number = choice.dataset['number'];
-        choice.querySelector(".choice-text").innerText = currentQuestion['choice' + number];
+        //@CC take the question at the index
+        choice.querySelector(".choice-text").innerText = currentQuestion.choices[idx];
+        idx++;
     });
-// This will stop showing the same questions again
+    // This will stop showing the same questions again
     availableQuestions.splice(questionIndex, 1);
 
     acceptingAnswers = true;
@@ -120,6 +148,12 @@ getNewQuestion = () => {
   if correct background will be green 
   if incorrect backgroud will be red
  */
+showImage = () => {
+    //@CC Hide Question . Show Image
+    gameContainer.style.display = "none"
+    imgContainer.src = currentQuestion.imageURL;
+    imgContainer.style.display = "block"
+}
 
 choices.forEach(choice => {
     choice.addEventListener('click', e => {
@@ -128,34 +162,38 @@ choices.forEach(choice => {
         const selectedChoice = e.currentTarget;
         const selectedAnswer = Number(selectedChoice.dataset.number);
 
+        //@CC added -1 becuas of arra starts at 0
         const classToApply =
-        selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
-        
+            selectedAnswer == currentQuestion.answer  ? 'correct' : 'incorrect'
 
-        if(classToApply === 'correct') {
-            incrementScore(correctBonus); 
+
+        if (classToApply === 'correct') {
+            incrementScore(correctBonus);
         }
 
-        
+
 
         selectedChoice.classList.add(classToApply);
 
-// This will wait 1 second to apply created classes below
-        setTimeout( () => {
-            selectedChoice.classList.remove(classToApply);
-            getNewQuestion();
-           
-        }, 1000)
+        // This will wait 1 second to apply created classes below
+        setTimeout(() => {
+            selectedChoice.classList.remove(classToApply)
+            showImage();
+            setTimeout(() => {
+                getNewQuestion();
+
+            }, 2000)
+
+        }, 800)
     });
 
 })
 
 // This will increment the users score when they answer correct
-function incrementScore(num) {    
-    score +=num;
+function incrementScore(num) {
+    score += num;
     scoreText.innerText = score;
 };
 
 
 startGame();
-
